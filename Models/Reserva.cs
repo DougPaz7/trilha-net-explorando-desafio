2ea1ddd0a0e6d255441885fc89ce30d2f7f10082
Douglas Paz <linodouglas321@gmail.com>
2024-07-30 13:23:16 -0300

    CódigoLimpo

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,16 +17,12 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
             if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
                 throw new ArgumentException("Capacidade menor que número de hóspedes recebido.");    
             }
         }
@@ -38,20 +34,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
             decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados >= 10)
             {
                 valor = valor - (valor / 10);
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
 Pessoa p1 = new Pessoa(nome: "Hóspede 1");
@@ -14,10 +13,8 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 hospedes.Add(p3);
 
-// Cria a suíte
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
@@ -28,6 +25,5 @@ Console.WriteLine("| HOTEL OLINDA BEACH |");
 Console.WriteLine("|                    |");
 Console.WriteLine("----------------------");
 
-// Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
